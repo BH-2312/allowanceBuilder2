d80376548b1889a88c1e6bbaf3733ab4a7e1ea07
Ben Hilliard <benhilliard23@hotmail.com>
2020-10-15 10:25:03 +1030

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Grug2020:Hotdog88@allowancebuilder2.5p3m0.mongodb.net/allowancebuilder2?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
+
